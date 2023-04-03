393e5f279fb79b518efa1a2d678c9938f188373c
letyresina <leticiaresina@gmail.com>
2023-04-03 10:51:09 -0300

    Entendendo Incremento e Decremento

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,4 @@
 // Switch = estrutura de decisão
-
 let opcao = parseInt(prompt("Escolha uma opção: "));
 
 switch(opcao){
@@ -19,3 +18,16 @@ switch(opcao){
         document.write("Opção inválida")
     break;
 }
+
+// Incremento:
+let valor = 7
+console.log(valor) // Retorna 7
+valor++ //Incrementa 1 -> pode ser feito valor = valor + 1 ou valor += 1
+console.log(valor) // Retorna 8
+
+//Decremento:
+let valor2 = 6
+console.log(valor2) // Retorna 6
+valor2-- //Decrementa 1 -> pode ser feito valor = valor - 1 ou valor -= 1
+console.log(valor2) //Retorna 5
+
