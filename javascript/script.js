91e00c56351c0206d5aef03fbe5bea28cfa27ec3
letyresina <leticiaresina@gmail.com>
2023-04-03 11:07:03 -0300

    Entendendo estrutura de repetição

--- javascript/script.js
+++ javascript/script.js
@@ -25,9 +25,20 @@ console.log(valor) // Retorna 7
 valor++ //Incrementa 1 -> pode ser feito valor = valor + 1 ou valor += 1
 console.log(valor) // Retorna 8
 
-//Decremento:
+// Decremento:
 let valor2 = 6
 console.log(valor2) // Retorna 6
 valor2-- //Decrementa 1 -> pode ser feito valor = valor - 1 ou valor -= 1
 console.log(valor2) //Retorna 5
 
+// Estrutura de repetição
+
+let i;
+
+for(i = 0; i < 10; i++){
+    document.write("Agora i vale: " + i + "<br>");
+}
+
+/* Em que, declarando o contador i que vale inicialmente 0, enquanto i for < (menor) que 10 ele
+vai incrementar sempre + 1. Ele para no 9, pois 10 é igual a 10 e não menor que 10.
+*/
